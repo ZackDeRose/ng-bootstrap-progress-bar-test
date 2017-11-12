c8c24d4d16681819a5f98a9f05cb967a1881f03c
Zack DeRose <zack.derose@gmail.com>
2017-11-12 02:43:53 -0500

    unable to animate change in value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  green: number;
+  blue: number;
+  blueMax: number;
+
+  constructor() {
+    this.green = 0;
+    this.blue = 0;
+    this.blueMax = 1000;
+  }
+
+  addGreen(){
+    this.green += 10;
+  }
+
+  addBlue(){
+    for(let i=0; i<100; i++){
+      this.blue++;
+    }
+    for(let i=0; i<10; i++){
+      this.blue--;
+    }
+    this.blue++;
+  }
+  getBluePercentage(): number {
+    return this.blue *100 / this.blueMax;
+  }
 }
